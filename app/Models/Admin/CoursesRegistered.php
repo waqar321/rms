d0b708f679cd85f105d81a132dd99bfb8d3a8fa9
waqar321 <waqarmughal707@gmail.com>
2024-07-24 00:52:03 -0700

    updating user id not found issue

--- app/Models/Admin/CoursesRegistered.php
+++ app/Models/Admin/CoursesRegistered.php
@@ -11,12 +11,19 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class CoursesRegistered extends Model
 {
 
-    use HasFactory, SoftDeletes, CommonRelations;
+    // use HasFactory, SoftDeletes, CommonRelations;
+    use HasFactory, SoftDeletes;
+
     protected $table = 'course_registered';
     
     protected $fillable = [
         'course_id',
         'user_id',
     ];
+
+    public function user()
+    {
+        return $this->belongsTo(ecom_admin_user::class);
+    }
     
 }
