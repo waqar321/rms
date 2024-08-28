8462e17883a08a11616628ed93f05a7d6c0895cb
waqar321 <waqarmughal707@gmail.com>
2024-08-28 15:32:45 +0500

    added assessment api

--- app/Http/Controllers/AdminApis/LectureApiController.php
+++ app/Http/Controllers/AdminApis/LectureApiController.php
@@ -387,7 +387,7 @@ class LectureApiController extends Controller
     {
         return response()->json([
                 'status' => 404, 
-                'requestData' => ecom_lecture::where('id', $request->lecture_id)->exists()->ToSql(), 
+                'requestData' => ecom_lecture::where('id', $request->lecture_id)->toSql(), 
                 'message' => 'Lecture Not Found'
             ], 
             404
